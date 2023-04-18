a6548cccf40258c84e0558a564c78e5df7fabe94
itota <c0a2201899@edu.teu.ac.jp>
2023-04-18 17:21:21 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -11,22 +11,30 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     kk_images = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
 
+    bg_img_r = pg.transform.flip(bg_img, True, False)
+
     tmr = 0
-    by = 0
+
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        screen.blit(bg_img, [-(tmr % 1600), 0])
-        screen.blit(bg_img, [1600-(tmr % 1600), 0])
+
+     
+        x = tmr % 3200
+
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img_r, [1600 - x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         
 
-        screen.blit(kk_images[by % 2], [300, 200])
-        by += 1
+
+        screen.blit(kk_images[0 if (x% 100) > 50 else 1], [300, 200])
+        
         pg.display.update()
-        clock.tick(100)
+        clock.tick(500)
 
 
 
